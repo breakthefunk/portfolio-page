f6c00d7b7ac7e6ee301c04adee6c4c56a3274a83
breakthefunk <38259552+breakthefunk@users.noreply.github.com>
2018-08-24 14:01:41 +0200

    Update sketch.js

--- docs/assets/sketch.js
+++ docs/assets/sketch.js
@@ -125,7 +125,7 @@ class Star {
         ellipse(this.sx, this.sy, this.nr, this.nr);
   	}
   	update() {
-    	this.speed = map(mouseX, 0, width, 0, 4);
+    	this.speed = map(mouseX, 0, width, 0, 0);
      	this.z = this.z - this.speed;
      	if (this.z < 0.5) {
       	this.z = random(width);
